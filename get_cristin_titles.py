e935359ca35cbf1e4a0a26ae070c1f10f1ff047d
Sergey Alyaev <cobxo3bot@gmail.com>
2025-08-06 13:50:39 +0200

    Script to extract cristin titles

--- get_cristin_titles.py
+++ get_cristin_titles.py
@@ -1,21 +1,35 @@
+from urllib.parse import urlencode
+import requests
+import json
 
-baseurl = 'https://api.cristin.no/v2/'
+baseurl = 'https://api.cristin.no/v2'
 
-def get_entries(url_part=''):
-    results = requests.get(baseurl+'results'+'?'+url_part)
-    results_data = json.loads(results.content)
+def get_entries(author, year):
+    name_year_dict = {
+        'contributor': author,
+        'published_since': year,
+        'published_before': year
+    }
+    url_part = urlencode(name_year_dict)
+    responce = requests.get(f'{baseurl}/results?{url_part}')
+
+    results_data = json.loads(responce.content)
+    print(results_data)
+
+    return results_data
+
+def get_titles(entries):
+    titles = [result['title'][result['original_language']] for result in entries]
+    return titles
 
 
-find_name = {
-    'contributor': 'Sergey Alyaev'
-    # 'published_since': '2022'
-    # 'published_before': '2021'
-}
+if __name__ == "__main__":
+    results_data = get_entries('Sergey Alyaev', 2022)
+    titles = get_titles(results_data)
+    for i, title in enumerate(titles):
+        print(f'{i+1}. {title}')
 
-# find_project = {
-#     'title': "distinguish"
-# }
+    # for result in results_data:
+    #     print(result['title'][result['original_language']])
 
-url_part = urlencode(find_name)
 
-result = requests.get(baseurl + 'projects' + '?' + url_part)
