907b20e43c9cd44ca4aeb5e47be517d033936e59
Sergey Alyaev <cobxo3bot@gmail.com>
2025-08-07 11:18:30 +0200

    Corrected bingo related typos

--- create_bingo_terms.py
+++ create_bingo_terms.py
@@ -12,13 +12,13 @@ def extract_bingo_terms(keyword_dict, top_n = 20):
     print("Top terms")
 
     for i, term in enumerate(top_terms):
-        print(f'{i+1}. {term[0]}: {term[1]['count']}')
+        print(f"{i+1}. {term[0]}: {term[1]['count']}")
 
     sampled_terms = random.sample(one_off_terms, min(top_n-len(top_terms), len(one_off_terms)))
     print("Sampled rare terms")
 
     for i, term in enumerate(sampled_terms):
-        print(f'{i+len(top_terms)+1}. {term[0]}: {term[1]['count']}')
+        print(f"{i+len(top_terms)+1}. {term[0]}: {term[1]['count']}")
 
     result_terms = top_terms + sampled_terms
     terms_array = [term[0] for term in result_terms]
